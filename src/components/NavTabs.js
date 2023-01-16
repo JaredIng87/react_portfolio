e4d5c2df27efda442d582be5d0251df13fdeeaa9
JaredIng87 <jaredingersoll14@gmail.com>
2023-01-16 18:10:06 -0500

    added css files for styling

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/NavTabs.css';
 
 const pages = [
     {href: "home", pageName: "Home"},
--- src/components/pages/PageComponent.js
+++ src/components/pages/PageComponent.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import './styles/Header.css';
+import './styles/Section.css';
 
 export default function PageComponent(props) {
   return (
     <div>
+      <header className="header">
       <h1>{props.header}</h1>
+      </header>
+      <section className="section">
       <p>{props.content}</p>
+      </section>
     </div>
   );
 }
