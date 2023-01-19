e81e71737e0cee17b83638e7ab7269c80469219f
JaredIng87 <jaredingersoll14@gmail.com>
2023-01-19 11:06:39 -0500

    edit style

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,7 @@ export default function About() {
       <div className='about'>
         <img src="./styles/images/self.jpg" alt="Self"></img>
         <p>
-        My name is Jared Ingersoll, and I am a coding student based in Trenton, NJ.<br></br> 
+        My name is Jared Ingersoll. I am a 35 year old coding student based in Trenton, NJ.<br></br> 
         Here you will find various projects i have worked on, as well as contact information and a link to my resume.
         </p>
       </div>
