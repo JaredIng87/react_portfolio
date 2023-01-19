4c621bb9f0b9d8ce9bd708089d9844f15e7c96e0
JaredIng87 <jaredingersoll14@gmail.com>
2023-01-19 15:10:05 -0500

    add social links to footer. edit style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { SocialIcon } from 'react-social-icons';
 import './styles/Footer.css';
 
 function Footer() {
+
   return (
 <div>
       <footer className="footer">
+    <div className='social'>
+      <SocialIcon className='social' url="https://github.com/JaredIng87"  bgColor="#3a3a3a"/>
+      </div>
+      <div className='social'>
+      <SocialIcon url="https://www.instagram.com/"  bgColor="#3a3a3a"/>
+      </div>
       <h4>This app was made with React.<br></br>2023</h4>
       </footer>
     </div>
