a73b6f9a7abcb05cc0de57013ff5e4083b477ca3
JaredIng87 <jaredingersoll14@gmail.com>
2023-03-22 20:58:28 -0400

    style edit

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ import './styles/About.css';
 export default function About() {
     return (
       <div className='about'>
-        <img src="./styles/images/self.jpg" alt="Self"></img>
+        <img src="./styles/images/me.jpg" alt="me"></img>
         <p>
         My name is Jared Ingersoll, and I am a new web developer based in Trenton, NJ. I received my certification in full-stack web design from Rutgers' Continuing Education Program. Here you will find various projects i have worked on, as well as contact information and a link to my resume.
         </p>
